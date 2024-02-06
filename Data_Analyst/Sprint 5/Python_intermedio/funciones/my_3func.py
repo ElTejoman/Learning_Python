8353c5e9eb6e9f680d17ed07f95c4cc72ca4f5a4
ElTejoman <aespinoza.ascue@gmail.com>
2024-02-06 03:09:31 -0500

    integrated function

---
+++ Data_Analyst/Sprint 5/Python_intermedio/funciones/my_3func.py
@@ -0,0 +1,13 @@
+# my_func.py
+product_price = 12.0
+product_qty = 5
+
+
+def total_price():
+    print(f"Precio total: {price * qty}")
+
+
+total_price()
+
+
+# El error se esta haciendo a propósito.
